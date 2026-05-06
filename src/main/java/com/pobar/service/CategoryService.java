276c41fa7f1d708513b719373f9d748b1f43a7ab
Daniel <hpigu840@gmail.com>
2026-05-07 02:42:19 +0800

    chore: 刪除冗餘檔案（CategoryService、PageResult），加入 .gitignore 與 README

--- src/main/java/com/pobar/service/CategoryService.java
+++
@@ -1,4 +0,0 @@
-package com.pobar.service;
-
-public interface CategoryService {
-}
--- src/main/java/com/pobar/service/impl/CategoryServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.pobar.service.impl;
-
-import com.pobar.service.CategoryService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CategoryServiceImpl implements CategoryService {
-}
